7b4fb375496607201a3dfba466c28407ee55766c
bright_spark <markpassey@btinternet.com>
2019-03-26 12:23:35 +0000

    Changed box rendering to use quads for edges rather than lines

--- src/main/java/brightspark/landmanager/handler/ClientEventHandler.java
+++ src/main/java/brightspark/landmanager/handler/ClientEventHandler.java
@@ -1,20 +1,10 @@
 package brightspark.landmanager.handler;
 
-import brightspark.landmanager.LMConfig;
 import brightspark.landmanager.LandManager;
 import brightspark.landmanager.data.areas.Area;
 import brightspark.landmanager.data.areas.CapabilityAreas;
+import brightspark.landmanager.util.BoxRenderer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -62,75 +52,6 @@ public class ClientEventHandler
         return colour;
     }
 
-    private static void renderBox(Area area, double partialTicks)
-    {
-        //Get player's actual position
-        EntityPlayerSP player = mc.player;
-        double x = player.prevPosX + (player.posX - player.prevPosX) * partialTicks;
-        double y = player.prevPosY + (player.posY - player.prevPosY) * partialTicks;
-        double z = player.prevPosZ + (player.posZ - player.prevPosZ) * partialTicks;
-        //Render the box
-        GlStateManager.pushMatrix();
-        GlStateManager.enableAlpha();
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.glLineWidth(5f);
-        GlStateManager.disableTexture2D();
-        GlStateManager.disableLighting();
-        GlStateManager.translate(-x, -y, -z);
-        float[] rgb = getColour(area.getName()).getRGBColorComponents(null);
-        AxisAlignedBB box = new AxisAlignedBB(area.getMinPos(), area.getMaxPos().add(1, 1, 1)).grow(0.001d);
-        GlStateManager.enableDepth();
-        RenderGlobal.renderFilledBox(box, rgb[0], rgb[1], rgb[2], 0.2f);
-        GlStateManager.disableDepth();
-        RenderGlobal.drawSelectionBoundingBox(box, rgb[0], rgb[1], rgb[2], 0.4f);
-        Vec3d playerPos = player.getPositionEyes((float) partialTicks);
-        Vec3d nameRenderPos = box.getCenter();
-        if(playerPos.y < box.minY + 0.5d)
-            nameRenderPos = new Vec3d(nameRenderPos.x, box.minY + 0.5d, nameRenderPos.z);
-        else if(playerPos.y > box.maxY - 0.5d)
-            nameRenderPos = new Vec3d(nameRenderPos.x, box.maxY - 0.5d, nameRenderPos.z);
-        else
-            nameRenderPos = new Vec3d(nameRenderPos.x, playerPos.y, nameRenderPos.z);
-        renderName(area, nameRenderPos);
-        GlStateManager.enableTexture2D();
-        GlStateManager.enableLighting();
-        GlStateManager.enableDepth();
-        GlStateManager.popMatrix();
-    }
-
-    //Copied a lot of this from EntityRenderer#drawNameplate and changed for my needs
-    private static void renderName(Area area, Vec3d center)
-    {
-        RenderManager rm = mc.getRenderManager();
-        float viewerYaw = rm.playerViewY;
-        float viewerPitch = rm.playerViewX;
-        boolean isThirdPersonFrontal = rm.options.thirdPersonView == 2;
-
-        GlStateManager.translate(center.x, center.y, center.z);
-        GlStateManager.glNormal3f(0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(-viewerYaw, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate((float) (isThirdPersonFrontal ? -1 : 1) * viewerPitch, 1.0F, 0.0F, 0.0F);
-        float scale = 0.04f * LMConfig.client.areaNameScale;
-        GlStateManager.scale(-scale, -scale, scale);
-        GlStateManager.disableTexture2D();
-
-        FontRenderer fr = mc.fontRenderer;
-        String name = area.getName();
-        int i = fr.getStringWidth(name) / 2;
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuffer();
-        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
-        bufferbuilder.pos((double) (-i - 1), -1D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        bufferbuilder.pos((double) (-i - 1), 8D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        bufferbuilder.pos((double) (i + 1), 8D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        bufferbuilder.pos((double) (i + 1), -1D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture2D();
-        fr.drawString(name, -i, 0, -1);
-        GlStateManager.disableBlend();
-    }
-
     @SubscribeEvent
     public static void areaRendering(RenderWorldLastEvent event)
     {
@@ -144,13 +65,13 @@ public class ClientEventHandler
         if(renderAll)
         {
             Set<Area> areas = cap.getNearbyAreas(mc.player.getPosition());
-            areas.forEach(area -> renderBox(area, event.getPartialTicks()));
+            areas.forEach(area -> BoxRenderer.renderBox(area, getColour(area.getName()), event.getPartialTicks()));
         }
         else if(!renderArea.isEmpty())
         {
             Area area = cap.getArea(renderArea);
             if(area != null)
-                renderBox(area, event.getPartialTicks());
+                BoxRenderer.renderBox(area, getColour(area.getName()), event.getPartialTicks());
         }
     }
 }
---
+++ src/main/java/brightspark/landmanager/util/BoxRenderer.java
@@ -0,0 +1,222 @@
+package brightspark.landmanager.util;
+
+import brightspark.landmanager.LMConfig;
+import brightspark.landmanager.data.areas.Area;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BoxRenderer
+{
+	private static final double EDGE_THICKNESS = 0.025D;
+	private static final Minecraft MC = Minecraft.getMinecraft();
+	private static final Map<EnumFacing, List<Triple<Vec3d, Vec3d, Vec3d>>> OFFSETS = new HashMap<>();
+
+	static
+	{
+		// https://minecraft.gamepedia.com/File:Minecraft_axes.png
+		/*
+		From UP:
+					North (-Z)
+		West (-X)				East (+X)
+					South (+Z)
+
+		From DOWN:
+					South (+Z)
+		West (-X)				East (+X)
+					North (-Z)
+
+		From NORTH:
+					UP (+Y)
+		East (+X)				West (-X)
+					DOWN(-Y)
+
+		From SOUTH:
+					UP (+Y)
+		West (-X)				East (+X)
+					DOWN(-Y)
+
+		From EAST:
+					UP (+Y)
+		South (+Z)				North (-Z)
+					DOWN(-Y)
+
+		From WEST:
+					UP (+Y)
+		North (-Z)				South (+Z)
+					DOWN(-Y)
+		*/
+		//First vector is the inside offset
+		//Second is the outside offset
+		//Third is the other inside offset for the internal faces
+		OFFSETS.put(EnumFacing.UP, Arrays.asList(
+			createVecTriple(1, 1, -1, -1, 1, 1, 1, -1, -1),     // South West -> -X, +Z
+			createVecTriple(-1, 1, -1, 1, 1, 1, -1, -1, -1),    // South East -> +X, +Z
+			createVecTriple(-1, 1, 1, 1, 1, -1, -1, -1, 1),     // North East -> +X, -Z
+			createVecTriple(1, 1, 1, -1, 1, -1, 1, -1, 1)));    // North West -> -X, -Z
+		OFFSETS.put(EnumFacing.DOWN, Arrays.asList(
+			createVecTriple(1, -1, 1, -1, -1, -1, 1, 1, 1),     // North West -> -X, -Z
+			createVecTriple(-1, -1, 1, 1, -1, -1, -1, 1, 1),    // North East -> +X, -Z
+			createVecTriple(-1, -1, -1, 1, -1, 1, -1, 1, -1),   // South East -> +X, +Z
+			createVecTriple(1, -1, -1, -1, -1, 1, 1, 1, -1)));  // South West -> -X, +Z
+		OFFSETS.put(EnumFacing.NORTH, Arrays.asList(
+			createVecTriple(-1, 1, -1, 1, -1, -1, -1, 1, 1),    // Down East -> +X, -Y
+			createVecTriple(1, 1, -1, -1, -1, -1, 1, 1, 1),     // Down West -> -X, -Y
+			createVecTriple(1, -1, -1, -1, 1, -1, 1, -1, 1),    // Up West -> -X, +Y
+			createVecTriple(-1, -1, -1, 1, 1, -1, -1, -1, 1))); // Up East -> +X, +Y
+		OFFSETS.put(EnumFacing.SOUTH, Arrays.asList(
+			createVecTriple(1, 1, 1, -1, -1, 1, 1, 1, -1),      // Down West -> -X, -Y
+			createVecTriple(-1, 1, 1, 1, -1, 1, -1, 1, -1),     // Down East -> +X, -Y
+			createVecTriple(-1, -1, 1, 1, 1, 1, -1, -1, -1),    // Up East -> +X, +Y
+			createVecTriple(1, -1, 1, -1, 1, 1, 1, -1, -1)));   // Up West -> -X, +Y
+		OFFSETS.put(EnumFacing.EAST, Arrays.asList(
+			createVecTriple(1, 1, -1, 1, -1, 1, -1, 1, -1),     // Down South -> +Z, -Y
+			createVecTriple(1, 1, 1, 1, -1, -1, -1, 1, 1),      // Down North -> -Z, -Y
+			createVecTriple(1, -1, 1, 1, 1, -1, -1, -1, 1),     // Up North -> -Z, +Y
+			createVecTriple(1, -1, -1, 1, 1, 1, -1, -1, -1)));  // Up South -> +Z, +Y
+		OFFSETS.put(EnumFacing.WEST, Arrays.asList(
+			createVecTriple(-1, 1, 1, -1, -1, -1, 1, 1, 1),     // Down North -> -Z, -Y
+			createVecTriple(-1, 1, -1, -1, -1, 1, 1, 1, -1),    // Down South -> +Z, -Y
+			createVecTriple(-1, -1, -1, -1, 1, 1, 1, -1, -1),   // Up South -> +Z, +Y
+			createVecTriple(-1, -1, 1, -1, 1, -1, 1, -1, 1)));  // Up North -> -Z, +Y
+	}
+
+	private static Triple<Vec3d, Vec3d, Vec3d> createVecTriple(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3)
+	{
+		return new ImmutableTriple<>(new Vec3d(x1, y1, z1), new Vec3d(x2, y2, z2), new Vec3d(x3, y3, z3));
+	}
+
+	public static void renderBox(Area area, Color colour, double partialTicks)
+	{
+		//Get player's actual position
+		EntityPlayerSP player = MC.player;
+		double x = player.prevPosX + (player.posX - player.prevPosX) * partialTicks;
+		double y = player.prevPosY + (player.posY - player.prevPosY) * partialTicks;
+		double z = player.prevPosZ + (player.posZ - player.prevPosZ) * partialTicks;
+		//Render the box
+		GlStateManager.pushMatrix();
+		GlStateManager.enableAlpha();
+		GlStateManager.enableBlend();
+		GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+		GlStateManager.glLineWidth(5f);
+		GlStateManager.disableTexture2D();
+		GlStateManager.disableLighting();
+		GlStateManager.translate(-x, -y, -z);
+		float[] rgb = colour.getRGBColorComponents(null);
+		AxisAlignedBB box = new AxisAlignedBB(area.getMinPos(), area.getMaxPos().add(1, 1, 1)).grow(0.001d);
+		GlStateManager.enableDepth();
+		RenderGlobal.renderFilledBox(box, rgb[0], rgb[1], rgb[2], 0.2f);
+		GlStateManager.disableDepth();
+		GlStateManager.color(rgb[0], rgb[1], rgb[2], 1f);
+		renderBoxEdges(box);
+		GlStateManager.color(1f, 1f, 1f);
+		Vec3d playerPos = player.getPositionEyes((float) partialTicks);
+		Vec3d nameRenderPos = box.getCenter();
+		nameRenderPos = new Vec3d(nameRenderPos.x, MathHelper.clamp(playerPos.y, box.minY + 0.5d, box.maxY - 0.5d), nameRenderPos.z);
+		renderName(area, nameRenderPos);
+		GlStateManager.enableTexture2D();
+		GlStateManager.enableLighting();
+		GlStateManager.enableDepth();
+		GlStateManager.popMatrix();
+	}
+
+	private static void renderBoxEdges(AxisAlignedBB box)
+	{
+		Vec3d minXminYminZ = new Vec3d(box.minX, box.minY, box.minZ);
+		Vec3d minXminYmaxZ = new Vec3d(box.minX, box.minY, box.maxZ);
+		Vec3d minXmaxYminZ = new Vec3d(box.minX, box.maxY, box.minZ);
+		Vec3d maxXminYminZ = new Vec3d(box.maxX, box.minY, box.minZ);
+		Vec3d minXmaxYmaxZ = new Vec3d(box.minX, box.maxY, box.maxZ);
+		Vec3d maxXmaxYminZ = new Vec3d(box.maxX, box.maxY, box.minZ);
+		Vec3d maxXminYmaxZ = new Vec3d(box.maxX, box.minY, box.maxZ);
+		Vec3d maxXmaxYmaxZ = new Vec3d(box.maxX, box.maxY, box.maxZ);
+		renderBoxEdgesForSide(EnumFacing.UP, minXmaxYmaxZ, maxXmaxYmaxZ, maxXmaxYminZ, minXmaxYminZ);
+		renderBoxEdgesForSide(EnumFacing.DOWN, minXminYminZ, maxXminYminZ, maxXminYmaxZ, minXminYmaxZ);
+		renderBoxEdgesForSide(EnumFacing.NORTH, maxXminYminZ, minXminYminZ, minXmaxYminZ, maxXmaxYminZ);
+		renderBoxEdgesForSide(EnumFacing.SOUTH, minXminYmaxZ, maxXminYmaxZ, maxXmaxYmaxZ, minXmaxYmaxZ);
+		renderBoxEdgesForSide(EnumFacing.EAST, maxXminYmaxZ, maxXminYminZ, maxXmaxYminZ, maxXmaxYmaxZ);
+		renderBoxEdgesForSide(EnumFacing.WEST, minXminYminZ, minXminYmaxZ, minXmaxYmaxZ, minXmaxYminZ);
+	}
+
+	private static void renderBoxEdgesForSide(EnumFacing facing, Vec3d... corners)
+	{
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder buffer = tessellator.getBuffer();
+		List<Triple<Vec3d, Vec3d, Vec3d>> offsetByVertex = OFFSETS.get(facing);
+		//Outer
+		buffer.begin(GL11.GL_QUAD_STRIP, DefaultVertexFormats.POSITION);
+		for(int i = 0; i < 5; i++)
+		{
+			int actualI = i < 4 ? i : 0;
+			Triple<Vec3d, Vec3d, Vec3d> triple = offsetByVertex.get(actualI);
+			Vec3d v = corners[actualI].add(triple.getLeft().scale(EDGE_THICKNESS));
+			buffer.pos(v.x, v.y, v.z).endVertex();
+			v = corners[actualI].add(triple.getMiddle().scale(EDGE_THICKNESS));
+			buffer.pos(v.x, v.y, v.z).endVertex();
+		}
+		tessellator.draw();
+		//Inner
+		buffer.begin(GL11.GL_QUAD_STRIP, DefaultVertexFormats.POSITION);
+		for(int i = 0; i < 5; i++)
+		{
+			int actualI = i < 4 ? i : 0;
+			Triple<Vec3d, Vec3d, Vec3d> triple = offsetByVertex.get(actualI);
+			Vec3d v = corners[actualI].add(triple.getRight().scale(EDGE_THICKNESS));
+			buffer.pos(v.x, v.y, v.z).endVertex();
+			v = corners[actualI].add(triple.getLeft().scale(EDGE_THICKNESS));
+			buffer.pos(v.x, v.y, v.z).endVertex();
+		}
+		tessellator.draw();
+	}
+
+	//Copied a lot of this from EntityRenderer#drawNameplate and changed for my needs
+	private static void renderName(Area area, Vec3d center)
+	{
+		RenderManager rm = MC.getRenderManager();
+		float viewerYaw = rm.playerViewY;
+		float viewerPitch = rm.playerViewX;
+		boolean isThirdPersonFrontal = rm.options.thirdPersonView == 2;
+
+		GlStateManager.translate(center.x, center.y, center.z);
+		GlStateManager.glNormal3f(0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(-viewerYaw, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate((float) (isThirdPersonFrontal ? -1 : 1) * viewerPitch, 1.0F, 0.0F, 0.0F);
+		float scale = 0.04f * LMConfig.client.areaNameScale;
+		GlStateManager.scale(-scale, -scale, scale);
+		GlStateManager.disableTexture2D();
+
+		FontRenderer fr = MC.fontRenderer;
+		String name = area.getName();
+		int i = fr.getStringWidth(name) / 2;
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+		bufferbuilder.pos((double) (-i - 1), -1D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+		bufferbuilder.pos((double) (-i - 1), 8D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+		bufferbuilder.pos((double) (i + 1), 8D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+		bufferbuilder.pos((double) (i + 1), -1D, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+		tessellator.draw();
+		GlStateManager.enableTexture2D();
+		fr.drawString(name, -i, 0, -1);
+		GlStateManager.disableBlend();
+	}
+}
