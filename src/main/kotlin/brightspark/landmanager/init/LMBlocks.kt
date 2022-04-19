c9c7f12c7a7f1f75258328a226a56133307ff844
bright_spark <markpassey@btinternet.com>
2022-04-19 11:47:27 +0100

    Added missing Home block drop and properties

--- src/main/kotlin/brightspark/landmanager/init/LMBlocks.kt
+++ src/main/kotlin/brightspark/landmanager/init/LMBlocks.kt
@@ -4,6 +4,7 @@ import brightspark.landmanager.block.HomeBlock
 import brightspark.landmanager.util.setRegName
 import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
+import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
 import net.minecraftforge.event.RegistryEvent
 import thedarkcolour.kotlinforforge.forge.objectHolder
@@ -12,7 +13,7 @@ object LMBlocks {
 	val HOME: Block by objectHolder("home")
 
 	fun register(event: RegistryEvent.Register<Block>) = event.registry.register(
-		HomeBlock(props(Material.WOOD)).setRegName("home")
+		HomeBlock(props(Material.WOOD).hardnessAndResistance(2F, 5F).sound(SoundType.WOOD)).setRegName("home")
 	)
 
 	private fun props(material: Material): AbstractBlock.Properties = AbstractBlock.Properties.create(material)
